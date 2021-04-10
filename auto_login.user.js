42c3ce54ea5414d8ab52010270b93a70fea053a0
OfirKedem <ofirk101@gmail.com>
2021-04-10 16:09:06 +0300

    change script name

--- auto_login.user.js
+++ auto_login.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TAU-Moodle auto sing in
+// @name         TAU-Moodle auto login
 // @name:he           כניסה אוטומטית למודל
 // @namespace    http://tampermonkey.net/
 // @version      0.1
